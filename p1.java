4fee2be1c79a27dab2185db7888d5cc2c62386fa
Dilshanvir Singh Grewal <grewaldilshan19@gmail.com>
2025-10-07 20:15:53 +0530

    p1 new

--- p1.java
+++ p1.java
@@ -2,14 +2,14 @@ class Main
 {
     public static void main(String[] args)
     {
-        int[]a=new int[4];//length of array is determined
+        int[]a=new int[4];                      //length of array is determined
         a[0]=2;
         a[1]=7;
         a[2]=11;
         a[3]=15;
-        int s=9;//no we need as a sum
-        int c=0;//variable to store sum of no's
-        int f=0;//flag variable to check if no number is found
+        int s=9;                                //no we need as a sum
+        int c=0;                                //variable to store sum of no's
+        int f=0;                                //flag variable to check if no number is found
         for(int i=0;i<3;i++)
         {
             for(int j=i;j<4;j++)
