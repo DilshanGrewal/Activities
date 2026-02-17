5c964bc4ee1fc259c3e9e66dddac331d1afb4466
Dilshanvir Singh Grewal <grewaldilshan19@gmail.com>
2026-02-17 09:26:28 +0530

    p2

---
+++ p2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i=0;i<=10;i++)
+    {
+        cout<<i<<endl;
+    }
+    int j=0;
+    while(j<=10)
+    {
+        cout<<j<<endl;
+        j++;
+    }
+    int k=0;
+    do
+    {
+        cout<<k<<endl;
+        k++;
+    }while(k<=10);
+    return 0;
+}
